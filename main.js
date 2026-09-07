93dce47feddc9d5e0d25e7bfebe56c742e5d9b26
AskAlbert <Albert.Askenmalm@protonmail.com>
2026-09-07 12:48:02 +0200

    Added backbutton to the mobile layout and chagned the description for brush time

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const buttons= document.querySelectorAll("button");
 
 buttons.forEach((button)=>{
+    let showImg=true;
     button.addEventListener("click",()=>{
         const card=button.closest(".card");
         const image=card.querySelector(".card-img");
         const text=card.querySelector("p")
-        image.hidden=true;
-        text.style.display="block";
-        button.style.display="none"
+        
+        if(showImg===true){
+            image.hidden=true;
+            text.style.display="block";
+            button.innerText="\u{1F868}";
+            button.classList.add("back-button");
+            showImg=false;
+        }
+        else{
+            button.classList.remove("back-button");
+            image.hidden=false;
+            text.style.display="none";
+            button.innerText="Read more";
+            showImg=true;
+        }
     })
 })
