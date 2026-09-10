5942713fc525c28e3d207faa1593860756ed9962
AskAlbert <Albert.Askenmalm@protonmail.com>
2026-09-10 15:14:07 +0200

    Updated spelling and removed unused css

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const buttons= document.querySelectorAll("button");
 
 buttons.forEach((button)=>{
-    let showImg=true;
+    let imgVisible=true;
     button.addEventListener("click",()=>{
         const card=button.closest(".card");
         const image=card.querySelector(".card-img");
-        const text=card.querySelector("p")
+        const text=card.querySelector("p");
         
-        if(showImg===true){
+        if(imgVisible===true){
             image.hidden=true;
             text.style.display="block";
             button.innerText="\u{1F868}";
             button.classList.add("back-button");
-            showImg=false;
+            imgVisible=false;
         }
         else{
             button.classList.remove("back-button");
             image.hidden=false;
             text.style.display="none";
             button.innerText="Read more";
-            showImg=true;
+            imgVisible=true;
         }
     })
 })
